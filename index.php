7628485ea3081dda2854c69f4d7362bb579576c1
MemeWasTaken <memelazzari02@gmail.com>
2022-02-01 15:09:15 +0100

    upper header

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+    <title>Google Faq</title>
+</head>
+<body>
+    <header>
+        <div class="container-large">
+            <div class="upper-header">
+                <div class="logo">
+                    <img class="img-logo" src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Google">
+                    <h1 class="title-logo">Privacy e termini</h1>
+                </div>
+                <div class="user">
+                    <img src="nineDots.png" alt="">
+                    <div class="mask-profile">
+                        <img class="img-user" src="userImg.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-header"></div>
+        </div>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
